0376c0e78cae511001374a457e4c7dc846712673
Samax69 <vkvaishnav985@gmail.com>
2025-06-22 01:43:52 +0530

    first Initialize

---
+++ oops.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class Device{
+    protected:
+    string name;
+    bool status;
+
+
+};
+
+class Light{
+
+};
+
+class Fan{
+    private:
+    int speed;
+};
+
+class Room{
+    
+};
+
+class User{
+    int User_type;
+    string name;
+};
+
+class SHMS{
+
+};
+
+
+int main(){
+
+}
