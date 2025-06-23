50fe714223d003fa0f2e252d4647f8fbfe897766
Samax69 <vkvaishnav985@gmail.com>
2025-06-24 01:10:09 +0530

    add some variable and make classes

--- oops.cpp
+++ oops.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class Device{
@@ -6,29 +7,63 @@ class Device{
     string name;
     bool status;
 
+    public:
+    void showStatus();
+    
+
 
 };
 
-class Light{
+void Device :: showStatus(){
+    if(status == 1) {
+        cout << "Device is On" << endl;
+    }
+
+    else{
+        cout << "Device is Off" << endl;
+    }
+}
+
+class Light : public Device{
 
 };
 
-class Fan{
+class Fan : public Device{
     private:
     int speed;
-};
 
-class Room{
     
 };
 
-class User{
-    int User_type;
+class AC : public Device{
+
+};
+
+class Room{
+    string Room_name;
+    vector <Device*> devices;
+
+};
+
+class User{   // this should be my Abstract Class
+    int User_type;  // 1. Owner , 2. Residence , 3. Guest
     string name;
 };
 
-class SHMS{
+class Owner : public User{
 
+};
+
+class Resident: public User{
+
+};
+
+class Guest: public User{
+
+};
+
+class SHMS{
+    vector<Room*> rooms;
 };
 
 
